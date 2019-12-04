cdf0dbdc5f73521bce78bf942467b5d660032705
Jose Solano R <josesolano@outlook.com>
2019-12-03 22:33:02 -0400

    cambios para el nombre del sistema

--- RegistroDoc/Data/DbInitializer.cs
+++ RegistroDoc/Data/DbInitializer.cs
@@ -42,8 +42,8 @@ namespace RegistroDoc.Data
             var users = new User[]
             {
             new User{FirstName="Admin",LastName="Admin",SecondLastName="Admin", Email="admin@test.com", Account="Admin", Password=_hash.EncryptString("Admin123"), RoleId=1},
-            new User{FirstName="User1",LastName="User1",SecondLastName="User1", Email="user1@test.com", Account="User1", Password=_hash.EncryptString("User1"), RoleId=2},
-            new User{FirstName="User2",LastName="User2",SecondLastName="User2", Email="user2@test.com", Account="User2", Password=_hash.EncryptString("User2"), RoleId=3}
+            new User{FirstName="Usuario 1",LastName="Usuario 1",SecondLastName="Usuario 1", Email="user1@test.com", Account="Usuario 1", Password=_hash.EncryptString("User1"), RoleId=2},
+            new User{FirstName="Usuario 2",LastName="Usuario 2",SecondLastName="Usuario 2", Email="user2@test.com", Account="Usuario 2", Password=_hash.EncryptString("User2"), RoleId=3}
             };
             foreach (User user in users)
             {

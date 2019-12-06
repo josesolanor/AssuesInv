07b9143fc1ba5211898a006de1211172b7e1dc3f
Jose Solano R <josesolano@outlook.com>
2019-12-05 20:49:57 -0400

    Cambios para añadir manejo de usuarios

---
+++ RegistroDoc/Controllers/UsersController.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using RegistroDoc.Context;
+using RegistroDoc.Core;
+using RegistroDoc.Entities;
+
+namespace RegistroDoc.Controllers
+{
+    [Authorize(Roles = "Admin")]
+    public class UsersController : Controller
+    {
+        private readonly AppDBContext _context;
+        private readonly Hash _hash;
+
+        public UsersController(AppDBContext context, Hash hash)
+        {
+            _context = context;
+            _hash = hash;
+        }
+
+        // GET: Users
+        public async Task<IActionResult> Index()
+        {
+            var appDBContext = _context.User.Include(u => u.Role);
+            return View(await appDBContext.ToListAsync());
+        }
+
+        // GET: Users/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var user = await _context.User
+                .Include(u => u.Role)
+                .FirstOrDefaultAsync(m => m.UserId == id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            return View(user);
+        }
+
+        // GET: Users/Create
+        public IActionResult Create()
+        {
+            ViewData["RoleId"] = new SelectList(_context.Role, "RoleId", "RoleId");
+            return View();
+        }
+
+        // POST: Users/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(User user)
+        {
+            if (ModelState.IsValid)
+            {
+                user.Password = _hash.EncryptString(user.Password);
+                _context.Add(user);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["RoleId"] = new SelectList(_context.Role, "RoleId", "RoleId", user.RoleId);
+            return View(user);
+        }
+
+        // GET: Users/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var user = await _context.User.FindAsync(id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+            ViewData["RoleId"] = new SelectList(_context.Role, "RoleId", "RoleId", user.RoleId);
+            return View(user);
+        }
+
+        // POST: Users/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, User user)
+        {
+            if (id != user.UserId)
+            {
+                return NotFound();
+            }
+
+            var updateUser = _context.User.FirstOrDefault(v => v.UserId.Equals(id));
+
+            updateUser.FirstName = user.FirstName;
+            updateUser.LastName = user.LastName;
+            updateUser.SecondLastName = user.SecondLastName;
+            updateUser.Account = user.Account;
+            updateUser.Email = user.Email;
+            updateUser.RoleId = user.RoleId;
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(updateUser);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!UserExists(updateUser.UserId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["RoleId"] = new SelectList(_context.Role, "RoleId", "RoleId", user.RoleId);
+            return View(user);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> ChangePassword(string NewPassword, int UserIdSelected)
+        {
+            var user = _context.User.FirstOrDefault(v => v.UserId.Equals(UserIdSelected));
+
+            user.Password = _hash.EncryptString(NewPassword);
+
+            _context.Update(user);
+            await _context.SaveChangesAsync();
+
+            return RedirectToAction("Index", "Users");
+        }
+
+        // GET: Users/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var user = await _context.User
+                .Include(u => u.Role)
+                .FirstOrDefaultAsync(m => m.UserId == id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            return View(user);
+        }
+
+        // POST: Users/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var user = await _context.User.FindAsync(id);
+            _context.User.Remove(user);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool UserExists(int id)
+        {
+            return _context.User.Any(e => e.UserId == id);
+        }
+    }
+}

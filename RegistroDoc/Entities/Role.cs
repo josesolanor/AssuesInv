4cdf3914cdbe228ce060ab545809101d902c0cb7
Jose Solano R <josesolano@outlook.com>
2019-12-05 21:12:36 -0400

    Traduccion del Role

--- RegistroDoc/Entities/Role.cs
+++ RegistroDoc/Entities/Role.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
@@ -10,10 +11,13 @@ namespace RegistroDoc.Entities
     public class Role
     {
         [Column("SYS_INASES_INV_ROLE_ROLE_ID")]
+        [DisplayName("Id del Rol")]
         public int RoleId { get; set; }
         [Column("SYS_INASES_INV_ROLE_ROLE_VALUE")]
+        [DisplayName("Rol")]
         public string RoleValue { get; set; }
         [Column("SYS_INASES_INV_ROLE_ROLE_DESCRIPTION")]
+        [DisplayName("Descripcion del Rol")]
         public string RoleDescription { get; set; }
 
         public ICollection<User> Users { get; set; }

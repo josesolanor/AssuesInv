509f26df295e580585eeeb2c3b39902734c3745e
Jose Solano R <josesolano@outlook.com>
2020-08-19 23:48:49 -0400

    Revert "Funcionando Correctamente como antes"
    This reverts commit 7e5d224fa0bf0b67a66b9675d6ac7707d1f2e123.

--- RegistroDoc/Program.cs
+++ RegistroDoc/Program.cs
@@ -19,21 +19,6 @@ namespace RegistroDoc
 
             var host = CreateHostBuilder(args).Build();
 
-            using (var scope = host.Services.CreateScope())
-            {
-                var services = scope.ServiceProvider;
-                try
-                {
-                    var context = services.GetRequiredService<AppDBContext>();
-                    DbInitializer.Initialize(context);
-                }
-                catch (Exception ex)
-                {
-                    var logger = services.GetRequiredService<ILogger<Program>>();
-                    logger.LogError(ex, "An error occurred while seeding the database.");
-                }
-            }
-
             host.Run();
         }
 
